6c530bc43ab75f2661de70f26b96cd7f89b73ff4
Jesse Kinkead <jessek@allenai.org>
2016-10-17 11:02:07 -0700

    Helper function.

--- src/main/scala/org/allenai/plugins/Utilities.scala
+++ src/main/scala/org/allenai/plugins/Utilities.scala
@@ -1,7 +1,6 @@
 package org.allenai.plugins
 
-import sbt.{ inProjects, Def, ScopeFilter, Task }
-import sbt.Keys.{ buildDependencies, thisProjectRef }
+import sbt.{ inProjects, Artifact, Def, ModuleID, Keys, ScopeFilter, Task }
 
 /** Helper methods and values for building plugins. */
 object Utilities {
@@ -10,7 +9,34 @@ object Utilities {
     * locate all of the local jars your project depends on.
     */
   lazy val localDependencyFilter: Def.Initialize[Task[ScopeFilter]] = Def.task {
-    val localDependencies = buildDependencies.value.classpathTransitiveRefs(thisProjectRef.value)
+    val localDependencies =
+      Keys.buildDependencies.value.classpathTransitiveRefs(Keys.thisProjectRef.value)
     ScopeFilter(inProjects(localDependencies: _*))
+  }
+
+  /** Construct a unique jar name from a given module ID and artifact. This will be of the form:
+    * {{{
+    * "${module.organization}.${module.name}-" +
+    *   "${artifact.name}-${module.revision}-${artifact.classifier}".jar
+    * }}}
+    * Classifier will be dropped if it's unset. The artifact name will be dropped if it exactly
+    * matches the module name, and will have the module name stripped out regardless.
+    */
+  def buildJarName(module: ModuleID, artifact: Artifact): String = {
+    val jarName = new StringBuilder(module.organization).append('.').append(module.name)
+
+    // Elide the artifact name if it exactly matches the module name.
+    if (module.name != artifact.name && artifact.name.nonEmpty) {
+      // Replace any occurance of the module name, to remove redundancy.
+      val strippedArtifactName = artifact.name.replace(module.name, "")
+      jarName.append('-').append(strippedArtifactName)
+    }
+
+    jarName.append('-').append(module.revision)
+    if (artifact.classifier.nonEmpty) {
+      jarName.append('-').append(artifact.classifier)
+    }
+
+    jarName.append(".jar").toString
   }
 }
